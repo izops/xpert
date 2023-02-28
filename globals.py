de80b272d502491f9dfa4f1b78b937b80c9d5ec5
Ivan Zustiak <ivan.zustiak@zurich.com>
2023-02-28 11:56:24 +0100

    create globals file for storing public global variables

---
+++ globals.py
@@ -0,0 +1,12 @@
+# %% contains public constants for xperience automation
+
+# URLs
+# login URL
+STR_URL_LOGIN = 'https://my.xperience.app/zurich/login.jsp'
+
+# add absence URL
+STR_URL_ADD_ABSENCE = 'https://my.xperience.app/zurich/web/timesystem?'
+STR_URL_ADD_ABSENCE += '__mvcevent=absenceList&id=0'
+
+# user name
+STR_USER_DOMAIN = '@zurich.com'
