ca969deb4e7720a1f70f0f8949d6bd50b60322f0
Ivan Zustiak <ivan.zustiak@zurich.com>
2023-05-10 13:38:27 +0200

    prepare process management flow for further options

--- ui/absences_process.py
+++ ui/absences_process.py
@@ -19,12 +19,29 @@ def RunProcess(pintChoice):
     Outputs:
         - None, either one or two processes are run
     """
-    # analyze the calendar
-    if pintChoice != g.INT_UI_CHOICE_XPERIENCE_SUBMISSION:
+    # group choices based on flowchart
+    lstAbsenceSubmission = [
+        g.INT_UI_CHOICE_OUTLOOK_EXPORT,
+        g.INT_UI_CHOICE_FULL_SUBMISSION
+    ]
+
+    lstScraping = [
+        g.INT_UI_CHOICE_XPERIENCE_SCRAPE,
+        g.INT_UI_CHOICE_FULL_DOWNLOAD
+    ]
+
+    lstNoCredentials = [
+        g.INT_UI_CHOICE_OUTLOOK_EXPORT,
+        g.INT_UI_CHOICE_OUTLOOK_IMPORT
+    ]
+
+    # coordinate the process steps based on user choice
+    if pintChoice in lstAbsenceSubmission or pintChoice in lstScraping:
         # request the starting and ending point for the calendar analysis
         strDateStart = abi.strGetDate(g.STR_UI_REQUEST_DATE_START)
         strDateEnd = abi.strGetDate(g.STR_UI_REQUEST_DATE_END)
 
+    if pintChoice in lstAbsenceSubmission:
         # get the calendar convention
         strConvention = abi.strGetCalendarConvention()
 
@@ -34,24 +51,45 @@ def RunProcess(pintChoice):
         else:
             blnOfficeFocused = False
 
-    if pintChoice != g.INT_UI_CHOICE_OUTLOOK_EXPORT:
+    if pintChoice in lstScraping:
+        # get type of absence to scrape - TO BE DONE
+        pass
+
+    if pintChoice not in lstNoCredentials:
         # get xperience credentials
         strUserName = c.strGetUserName()
         strPassword = c.strGetPassword()
 
-    if pintChoice != g.INT_UI_CHOICE_XPERIENCE_SUBMISSION:
+    if pintChoice in lstAbsenceSubmission:
         # launch the calendar analysis
         ca.AnalyzeCalendar(strDateStart, strDateEnd, blnOfficeFocused)
 
         # inform the user about the process end
         print(g.STR_UI_CALENDAR_ANALYSIS_COMPLETE)
 
-    if pintChoice != g.INT_UI_CHOICE_OUTLOOK_EXPORT:
-        # run xperience process
+    if pintChoice in [
+        g.INT_UI_CHOICE_XPERIENCE_SUBMISSION,
+        g.INT_UI_CHOICE_FULL_SUBMISSION
+    ]:
+        # run absence submission process
         w.SubmitAbsences(strUserName, strPassword)
 
         # discard the password
         del strPassword
+
+    if pintChoice in [
+        g.INT_UI_CHOICE_XPERIENCE_SCRAPE,
+        g.INT_UI_CHOICE_FULL_DOWNLOAD
+    ]:
+        # scrape xperience data - TO BE DONE
+        pass
+
+    if pintChoice in [
+        g.INT_UI_CHOICE_OUTLOOK_IMPORT,
+        g.INT_UI_CHOICE_FULL_DOWNLOAD
+    ]:
+        # save xperience data to outlook - TO BE DONE
+        pass
 
     # the process ends here, say goodbye to the user
     print(g.STR_UI_GOODBYE)
