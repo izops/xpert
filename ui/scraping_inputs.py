816698e2085d89e3a4cf67555f50f6a6329dcb0d
Ivan Zustiak <ivan.zustiak@zurich.com>
2023-05-12 10:43:39 +0200

    get user input for absence type scraping

---
+++ ui/scraping_inputs.py
@@ -0,0 +1,36 @@
+# %% import modules
+import sys
+
+sys.path.append('../emea_oth_xpert')
+import general.global_constants as g
+
+# %% define functions for obtaining inputs for data scraping
+def strGetAbsenceType():
+    # set initial value
+    strResponse = None
+
+    # obtain from user valid answer, make difference between first
+    # and the rest of the inputs
+    while strResponse is None \
+    or (
+        strResponse.lower() != 'c' \
+        and not strResponse.lower() in g.LST_SCRAPE_ABSENCES
+    ):
+        # loop until either canceled, or valid absence is provided
+        if strResponse is None:
+            # user is prompted for input for the first time
+            strMessage = 'Please, select an absence to scrape from the '
+            strMessage += 'following list: \n'
+            strMessage += str(g.LST_SCRAPE_ABSENCES)
+        
+        elif strResponse.lower() != 'c' \
+        or not strResponse.lower() in g.LST_SCRAPE_ABSENCES:
+            # not the first input but not valid either
+            strMessage = 'Apologies, I cannot scrape "' + strResponse 
+            strMessage += '" absence for you. '
+            strMessage += 'Choose a different one or type "c" to cancel.\n'
+            
+        # prompt user for input
+        strResponse = input(strMessage)
+
+    return strResponse
