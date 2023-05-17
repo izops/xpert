a79e4b5fd5adcfd8c8b75785e89a64f2c26421be
Ivan Zustiak <ivan.zustiak@zurich.com>
2023-05-17 11:56:58 +0200

    add option to select scrape absence using an index

--- ui/scraping_inputs.py
+++ ui/scraping_inputs.py
@@ -1,8 +1,15 @@
 # %% import modules
+import logging
 import sys
 
 sys.path.append('../emea_oth_xpert')
 import general.global_constants as g
+
+# %% set up logging
+logging.basicConfig(
+    level = g.OBJ_LOGGING_LEVEL,
+    format=' %(asctime)s -  %(levelname)s -  %(message)s'
+)
 
 # %% define functions for obtaining inputs for data scraping
 def strGetAbsenceType():
@@ -20,22 +27,33 @@ def strGetAbsenceType():
     # prepare lowercase list of values to compare against
     lstAbsences = [x.lower() for x in g.LST_SCRAPE_ABSENCES]
 
+    # prepare list of indexes for alternative choice
+    lstIndexes = [str(x + 1) for x in range(len(lstAbsences))]
+
     # obtain from user valid answer, make difference between first
     # and the rest of the inputs
     while strResponse is None \
     or (
         strResponse != 'c' \
-        and not strResponse in lstAbsences
+        and not strResponse in lstAbsences \
+        and not strResponse in lstIndexes
     ):
+        # log the response
+        logging.debug('strGetAbsenceType - strResponse: ' + str(strResponse))
+
         # loop until either canceled, or valid absence is provided
         if strResponse is None:
             # user is prompted for input for the first time
             strMessage = 'Please, select an absence to scrape from the '
             strMessage += 'following list: \n'
-            strMessage += str(g.LST_SCRAPE_ABSENCES) + ':\n'
+            strMessage += str(g.LST_SCRAPE_ABSENCES) + '\n'
+            strMessage += 'or type in its respective index (for example '
+            strMessage += g.LST_SCRAPE_ABSENCES[0] + ' = ' + lstIndexes[0]
+            strMessage += '):\n'
         
         elif strResponse != 'c' \
-        or not strResponse in lstAbsences:
+        or not strResponse in lstAbsences \
+        or not strResponse in lstIndexes:
             # not the first input but not valid either
             strMessage = 'Apologies, I cannot scrape "' + strResponse 
             strMessage += '" absence for you. '
@@ -43,5 +61,10 @@ def strGetAbsenceType():
             
         # prompt user for input, make it lowercase
         strResponse = input(strMessage).lower()
+
+    # if the input was an index, convert it to an absence value
+    if strResponse.isnumeric():
+        # subtract one to get zero-based index for python
+        strResponse = g.LST_SCRAPE_ABSENCES[int(strResponse) - 1]
 
     return strResponse
